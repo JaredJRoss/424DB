8f031106bc172989f5216065eb3d90477d383521
JaredJRoss <jaredrossj@gmail.com>
2017-04-09 18:01:57 -0400

    changed models

--- DB/DAGR/models.py
+++ DB/DAGR/models.py
@@ -9,13 +9,25 @@ class DAGR(models.Model):
     LastModified = models.DateTimeField(_("Last Modified"))
     Size = models.CharField(_("Size"))
     DeletionTime = models.DateTimeField(_("Deletion Time"))
-    Category =  models.CharField(_("Category"))
+    Has_Kids = models.BooleanField(("Has Kids"))
 
+class Category(models.models):
+    Name = models.CharField(_("Name"))
+    ParentCategory = models.ForeignKey(_(Category))
 
 class Document(models.Model):
     Name = models.CharField(_("Name"))
+    Author = models.CharField(_("Author"))
+    CreationTime = models.DateTimeField(_("Creation Time"))
+    LastModified = models.DateTimeField(_("Last Modified"))
+    Size  = models.IntegerField(_("Size"))
     Links = models.CharField(_("Link"))
+    Type = models.CharField(_("Type"))
     Owner = models.ForeignKey(_(DAGR))
+
+class DAGR_Category(models.Model):
+    Category =  models.ForeignKey(_(Category))
+    DAGR =  models.ForeignKey(_(DAGR))
 
 
 class DAGR_Children(models.Model):
